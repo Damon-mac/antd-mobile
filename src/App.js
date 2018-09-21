b411a96dd4fb3e0d4b6b6cf50e2014f6ac05eb9a
Damon-mac <weidongzhu36@126.com>
2018-09-21 11:14:36 +0800

    新增

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MyCarousel from './components/Carousel/index'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MyCarousel></MyCarousel>
+      </div>
+    );
+  }
+}
+
+export default App;
